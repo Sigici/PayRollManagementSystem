b46f664bb10d579410d73b38bc13dfdad0633bb0
Lawani Succeed Michael <62074078+Sigici@users.noreply.github.com>
2023-10-28 17:21:37 +0100

    first commit

---
+++ PayRollManagementSystem/Models/Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayRollManagementSystem.Models    
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime PayDate { get; set; }
+        public double RegularHours { get; set; }
+        public double OvertimeHours { get; set; }
+
+        // Private fields for regular and overtime rates
+        private double regularRate;
+        private double overtimeRate;
+
+        // Public read-only properties for regular and overtime rates
+        public double RegularRate
+        {
+            get { return regularRate; }
+        }
+
+        public double OvertimeRate
+        {
+            get { return overtimeRate; }
+        }
+
+        public double GrossPay { get; private set; }
+        public double NetPay { get; private set; }
+        public double MedicareDeduction { get; private set; }
+        public double RentDeduction { get; private set; }
+        public double FoodDeduction { get; private set; }
+
+        public Employee (double regularRate, double overtimeRate)
+        {
+            this.regularRate = regularRate;
+            this.overtimeRate = overtimeRate;
+        }
+
+        public void CalculatePayroll()
+        {
+            if (IsActive)
+            {
+                // Calculate Gross Pay
+                GrossPay = (RegularHours * RegularRate) + (OvertimeHours * OvertimeRate);
+
+                // Calculate Deductions
+                MedicareDeduction = 0.02 * GrossPay;
+                RentDeduction = 0.05 * GrossPay;
+                FoodDeduction = 0.03 * GrossPay;
+
+                // Calculate Net Pay
+                NetPay = GrossPay - (MedicareDeduction + RentDeduction + FoodDeduction);
+            }
+        }
+
+    }
+}
---
+++ PayRollManagementSystem/Models/PayrollManager.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleTables;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayRollManagementSystem.Models
+{
+    public class PayrollManager
+    {
+
+        private List<Employee> employees = new List<Employee>();
+
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return employees;
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            List<Employee> employees = GetAllEmployees();
+
+            
+
+            Console.WriteLine("Employee Info");
+
+            var newTable = new ConsoleTable("Name", "Active", "Start Date", "End Date", "Pay Date", "Regular Hours", "Overtime Hours");
+            var newTable2 = new ConsoleTable("Regular Rate", "OverTime Rate", "Gross Pay", "Net Pay", "Medicare Deduction", "Rent Deduction", "Food Deduction"); 
+            foreach (var employee in employees)
+            {
+                employee.CalculatePayroll();
+                newTable.AddRow(employee.Name, employee.IsActive, employee.StartDate, employee.EndDate, employee.PayDate, employee.RegularHours, employee.OvertimeHours);
+                newTable2.AddRow(employee.RegularRate,employee.OvertimeRate, employee.GrossPay, employee.NetPay, employee.MedicareDeduction, employee.RentDeduction, employee.FoodDeduction);
+               
+            }
+
+            newTable.Options.EnableCount = false;
+            newTable2.Options.EnableCount = false;
+            newTable.Write();
+            newTable2.Write();
+        }
+    }
+}
